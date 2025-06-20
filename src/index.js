df15db1e0783db8ab2635ee0fe50ef5e96c6e05c
Sankalp Lele <sankalplele2003@gmail.com>
2025-06-20 15:13:06 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// require("dotenv").config({ path: "./env" });
+// we can use the require syntax for dot env but since we want to be consistent with the use of import statements we must find a way to use import statements for doing the same
+//we do this by changing the 'dev' command in package.json and use an experimental feature
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+/*
+import express from "express";
+const app = express;
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("ERROR: ", error);
+    throw err;
+  }
+})();
+*/
